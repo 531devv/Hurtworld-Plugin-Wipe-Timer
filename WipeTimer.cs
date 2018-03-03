c14d82ced55240a80986539df40c5f1abef6fdaf
Marek <531devv@gmail.com>
2018-03-03 13:10:44 +0100

    Update to universal plugin and add lang api

--- WipeTimer.cs
+++ WipeTimer.cs
@@ -1,18 +1,13 @@
-﻿// Reference: Oxide.Core.MySql
-
-using System;
-using System.IO;
+﻿using System;
 using System.Collections.Generic;
-using System.Timers;
 using Oxide.Core.Libraries.Covalence;
-using Oxide.Core;
 
 namespace Oxide.Plugins
 {
-    [Info("WipeTimer", "531devv", "1.0.1", ResourceId = 531)]
-    [Description("You can check the time to wipe")]
+    [Info("WipeTimer", "531", "1.0.1", ResourceId = 0)]
+    [Description("You can check when is wipe")]
 
-    class WipeTimer : HurtworldPlugin
+    class WipeTimer : CovalencePlugin
     {
         DefaultConfig config;
 
@@ -36,9 +31,19 @@ namespace Oxide.Plugins
             SaveConfig();
         }
 
-        #endregion
 
-        void Init()
+        void LoadDefaultMessages()
+        {
+            //English
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["CommandWipe"] = "Time to wipe:"
+            }, this);
+        }
+
+    #endregion
+
+    void Init()
         {
             try
             {
@@ -49,21 +54,23 @@ namespace Oxide.Plugins
                 PrintWarning("Could not read config, creating new default config");
                 LoadDefaultConfig();
             }
+            LoadDefaultMessages();
         }
 
-        [ChatCommand("wipe")]
-        void cmdWipe(PlayerSession p, string command, string[] args)
+        [Command("wipe")]
+        void cmdWipe(IPlayer p, string command, string[] args)
         {
             DateTime date1 = new DateTime(config.year, config.month, config.day, config.hour, config.min, config.sec);
             System.TimeSpan diff1 = date1.Subtract(DateTime.Now);
 
             string time = string.Format("{0}d:{1}h:{2}m", diff1.Days, diff1.Hours, diff1.Minutes);
-
-            hurt.SendChatMessage(p, "<color=#FF0000>Do konca wipe zostalo:</color>");
-            hurt.SendChatMessage(p, time);
-
+            p.Reply("<color=#ff0000ff>" + Lang("CommandWipe", null) + "</color>");
+            p.Reply(time);
         }
+
         T GetConfig<T>(string name, T value) => Config[name] == null ? value : (T)Convert.ChangeType(Config[name], typeof(T));
+
+        private string Lang(string key, string id = null, params object[] args) => string.Format(lang.GetMessage(key, this, id), args);
     }
 }
 
